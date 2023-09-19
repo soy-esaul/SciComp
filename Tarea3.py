214fce60b78f6ffccade806facad363c0127c117
Juan Esaul <esauldd@gmail.com>
2023-09-18 18:35:20 -0600

    Finished code for homework 3

--- Tarea3.py
+++ Tarea3.py
@@ -1,38 +1,83 @@
-# %%
-                           ###### Problema 1 ######
-# Recquired packages
-import numpy as np
-from QR import QR, LSQR
-from LUyCholesky import LUP, cholesky, backsubs, forsubs
-from scipy.stats import norm as N
-from scipy.linalg import cholesky as schol
-from time import time
+def compare(X,beta,sigma=0.13):
+    '''This function gets the Least Squares Estimator for a regression problem with controlled
+    noise using two diffeent methods and adding aproximation error. It is designed to compare
+    how different perturbations of input and output con modify the estimation of hat beta with
+    each method'''
+    import numpy as np
+    from scipy.stats import norm as N
+    from scipy.linalg import inv
+    from QR import LSQR
+    
+    epsilon = N.rvs(loc=0, scale=sigma, size=n)
+    y = X @ beta + epsilon
+    DeltaX = N.rvs(loc=0,scale=0.01,size=d*n).reshape((n,d))
 
-A = N.rvs(scale=1, loc=0, size=400)
-A = A.reshape((20,20))
+    hat_beta = LSQR(X,y)
+    hat_beta_p = LSQR(X + DeltaX, y)
+    hat_beta_c = ( inv( (X + DeltaX).T @ (X + DeltaX) ) @ (X + DeltaX).T ) @ y
+    
+    return hat_beta, hat_beta_p, hat_beta_c
+if __name__ == "__main__":
+    # %%
+                            ###### Problema 1 ######
+    # Recquired packages
+    import numpy as np
+    from QR import QR, LSQR
+    from LUyCholesky import LUP, cholesky, backsubs, forsubs
+    from scipy.stats import norm as N
+    from scipy.stats import uniform
+    from scipy.linalg import inv
+    from scipy.linalg import cholesky as schol
+    from time import time
 
-Q, R = QR(A)
+    A = N.rvs(scale=1, loc=0, size=400)
+    A = A.reshape((20,20))
 
-lambdas = [9e15,8e14,7e13,6e12,5e11,4e10,3e9,2e8,2e7,2e6,2e5,2e4,2e3,2e2,60,5,4,3,2,1]
-epsilon = N.rvs(scale=0.01, loc=0, size=20)
+    Q, R = QR(A)
 
-B = (Q.T @ np.diag(lambdas) ) @ Q
-Be = (Q.T @ (np.diag(lambdas + epsilon))) @ Q
+    lambda1 = 5e13
+    ratio = 20
 
-start = time()
-Bchol = cholesky(B)
-Bechol = cholesky(Be)
-end = time()
+    lambdas_malas = [i/ratio for i in np.linspace(lambda1,ratio,num=20) ]
+    lambdas_buenas = np.linspace(20,1,num=20)
 
-t_hands = end - start
+    epsilon = N.rvs(scale=0.01, loc=0, size=20)
 
-start = time()
-sBchol = schol(B)
-sBechol = schol(Be)
-end = time()
+    B = (Q.T @ np.diag(lambdas) ) @ Q
+    Be = (Q.T @ (np.diag(lambdas + epsilon))) @ Q
 
-t_scipy = end - start
+    start = time()
+    Bchol = cholesky(B)
+    Bechol = cholesky(Be)
+    end = time()
 
-print(t_hands, t_scipy)
-# %%
-                           ###### Problema 2 ######
+    t_hands = end - start
+
+    start = time()
+    sBchol = schol(B,overwrite_a=True, check_finite=False)
+    sBechol = schol(Be,overwrite_a=True, check_finite=False)
+    end = time()
+
+    t_scipy = end - start
+
+    print(t_hands, t_scipy)
+    # %%
+                            ###### Problema 2 ######
+    d = 5
+    n = 20
+
+    beta = np.array([5,4,3,2,1])
+    sigma = 0.13
+
+    X = uniform.rvs(size = d*n)
+    X = X.reshape((n,d))
+
+    hat_beta, hat_beta_p, hat_beta_c = compare(X,beta,sigma)
+    
+    # %%
+    X_mala = uniform.rvs(size = int(3*n)).reshape((n,3))
+    ncol1 = 0.5*X_mala[:,0] + 0.5*X_mala[:,2] + N.rvs(loc=0,scale=0.00001,size=20)
+    ncol2 = 0.5*X_mala[:,1] + 0.5*X_mala[:,2] + N.rvs(loc=0,scale=0.00001,size=20)
+    X_mala = np.c_[X_mala, ncol1,ncol2]
+
+    hat_beta_mala, hat_beta_p_mala, hat_beta_c_mala = compare(X_mala,beta,sigma)
