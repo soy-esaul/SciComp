431ba842c05280bea01853ea869702f437173b94
Juan Esaul <esauldd@gmail.com>
2023-10-07 21:37:13 -0600

    Added functions for homework 5

--- Tarea 5.py
+++ Tarea 5.py
@@ -1,4 +1,4 @@
-def mudunif(n,seed=[57,189,42,26,4]):
+def simulate_unif(n,seed=[57,189,42,26,4]):
     '''This function simulates a sample of n i.i.d. random variables distributed
     as uniform in (0,1)
     
@@ -10,13 +10,37 @@ def mudunif(n,seed=[57,189,42,26,4]):
     - u: A vector with the simulated values (numpy array of shape (1,n))'''
     import numpy as np
     seed = np.asarray(seed, dtype=float)
-    x = np.zeros((n,1))
+    uniform_vector = np.zeros((n,1))
     for i in range(n):
-        x[i] = (107374182*seed[4] + 104420*seed[0]) % (2**(31) - 1)
+        uniform_vector[i] = (107374182*seed[4] + 104420*seed[0]) % (2**(31) - 1)
         seed = np.append(seed[1:],x[i])
-    return x / (2**(31) - 2)
+    return uniform_vector / (2**(31) - 2)
 # Simulation of exponential e = -log(u)
-def ARS()
+
+def log_gamma_dens(point,alpha=2,beta=1):
+    '''This function evaluates the logarithm of a Gamma(alpha,beta) density at 
+    a given point'''
+    import numpy as np
+    from scipy.stats import gamma
+    from scipy.special import gamma as Gamma
+    return (alpha*np.log(beta) - np.log(Gamma(alpha)) + (alpha-1)*np.log(point) - beta*point)
+
+def create_line(start,end,point_to_evaluate):
+    '''This function finds the function of a straight line passing through two points
+    and evaluates it at a given value of x
+    
+    Arguments:
+    - start: A 2-tuple with the x and y coordinates of the first point to find the line
+    - end: A 2-tuple with the x and y coordinates of the second point to find the line'''
+    return ((end[1]-start[1])*point_to_evaluate + start[1]*end[0] - start[0]*end[1] )/ (end[0] - start[0])
+
+def envelope(points):
+    '''This function creates an envelope for the logarithm of a Gamma density
+    
+    Arguments:
+    - points: A list or array-like containing the set of points to be used 
+    in the envelope'''
+    
 if __name__ == "__main__":
     import numpy as np
     from matplotlib import pyplot as plt
