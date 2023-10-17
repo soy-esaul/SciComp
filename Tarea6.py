15505b970b01ebdff708c5359f782dbfe75c8770
Juan Esaul <esauldd@gmail.com>
2023-10-17 15:12:18 -0600

    Corrected a mistake

--- Tarea6.py
+++ Tarea6.py
@@ -17,7 +17,7 @@ def rho_beta(x,y):
     rho = np.min([1,np.cos(np.pi*y)/np.cos(np.pi*x)])
     return rho
 
-def MH_beta(iterations,trials,successes,history=False):
+def MH_beta(iterations,trials,successes):
     '''This function implements the Metropolis-Hastings algorithm to simulate from 
     the posterior of the parameter in Bayesian inference'''
     import numpy as np
@@ -25,26 +25,15 @@ def MH_beta(iterations,trials,successes,history=False):
     current_point = np.random.random()*0.5
     output = []
     output.append(current_point)
-    if history == False:
-        for i in range(iterations):
-            proposal = np.random.beta(successes+1,trials-successes+1)
-            rho = rho_beta(current_point,proposal)
-            u = np.random.random()
-            if u <= rho:
-                current_point = proposal
-                output.append(current_point)
-        return output
-    else:
-        history = []
-        for i in range(iterations):
-            proposal = np.random.beta(successes+1,trials-successes+1)
-            history.append(current_point)
-            rho = rho_beta(current_point,proposal)
-            u = np.random.random()
-            if u <= rho:
-                current_point = proposal
-                output.append(current_point)
-        return output, history
+    for i in range(iterations):
+        proposal = np.random.beta(successes+1,trials-successes+1)
+        rho = rho_beta(current_point,proposal)
+        u = np.random.random()
+        if u <= rho:
+            current_point = proposal
+        output.append(current_point)
+    return output
+    
 
 def rho_unif(x,y,n,r):
     '''This function evaluates the probability of acceptance for the proposal in the 
@@ -62,32 +51,31 @@ def MH_unif(iterations,trials,successes,history=False):
     current_point = np.random.random()*0.5
     output = []
     output.append(current_point)
-    if history == False:
-        for i in range(iterations):
-            proposal = np.random.random()
-            rho = rho_unif(current_point,proposal,trials,successes)
-            u = np.random.random()
-            if u <= rho:
-                current_point = proposal
-                output.append(current_point)
-        return output
-    else:
-        history = []
-        for i in range(iterations):
-            proposal = np.random.random()
-            history.append(current_point)
-            rho = rho_unif(current_point,proposal,trials,successes)
-            u = np.random.random()
-            if u <= rho:
-                current_point = proposal
-                output.append(current_point)
-        return output, history
+    for i in range(iterations):
+        proposal = np.random.random()
+        rho = rho_unif(current_point,proposal,trials,successes)
+        u = np.random.random()
+        if u <= rho:
+            current_point = proposal
+        output.append(current_point)
+    return output
+    
 
 # Examples and homework
 if  __name__ == "__main__":
     import numpy as np
-    import matplotlib.pyplot as plt
     np.random.seed(57)
+   
+    # For LaTeX graphs
+    import matplotlib.pyplot as plt
+    import matplotlib
+    # matplotlib.use("pgf")
+    matplotlib.style.use("seaborn-v0_8")
+    # plt.rcParams.update({
+    # "pgf.texsystem": "pdflatex",
+    # 'font.family': 'serif',
+    # 'text.usetex': True,
+    # 'pgf.rcfonts': False,})
     
     # Samples of Bernoulli
     sample5 = np.random.binomial(5,1/3,1)
@@ -96,15 +84,27 @@ if  __name__ == "__main__":
     r40 = np.sum(sample40)
 
     # Implementation with data
-    MH_beta_sims5, history_beta_5 = MH_beta(1000,5,r5,history=True)
-    MH_beta_sims40, history_beta_40 = MH_beta(1000,40,r40,history=True)
+    MH_beta_sims5 = MH_beta(1000,5,r5)
+    MH_beta_sims40 = MH_beta(1000,40,r40)
     beta_sizes = [len(MH_beta_sims5), len(MH_beta_sims40)]
-    # Histograms
+    
+    # Plots of sampling process
+    fig_beta5, axb5 = plt.subplots(figsize=(7,4))
+    fig_beta40, axb40 = plt.subplots(figsize=(7,4))
+
+    axb5.plot(MH_beta_sims5)
+    axb40.plot(MH_beta_sims40)
 
     # Implementation with the new proposal
-    MH_unif_sims5, history_unif_5 = MH_unif(1000,5,r5,history=True)
-    MH_unif_sims40, history_unif_40 = MH_unif(1000,40,r40,history=True)
+    MH_unif_sims5 = MH_unif(1000,5,r5)
+    MH_unif_sims40 = MH_unif(1000,40,r40)
     unif_sizes = [len(MH_unif_sims5),len(MH_unif_sims40)]
+
+    fig_unif40, axu40 = plt.subplots(figsize=(7,4))
+    fig_unif5, axu5 = plt.subplots(figsize=(7,4))
+
+    axu5.plot(MH_unif_sims5)
+    axu40.plot(MH_unif_sims40)
 
     # Simple example of MH_beta()
     simple_sample = MH_beta(100,50,28)
