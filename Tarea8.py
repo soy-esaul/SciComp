0ddd3eceb995701a3cb1b0d4a3aa5d750b0bb28b
Juan Esaul <esauldd@gmail.com>
2023-12-03 23:44:35 -0600

    "Completed code for homework 8"

--- Tarea8.py
+++ Tarea8.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Functions
 ## For exercise 1
@@ -10,6 +11,12 @@ def logdensmvn(x,mu,Sigma=np.array([[1,0],[0,1]],dtype=float)):
 
 def logdensnorm(x,mu,sigma):
     return (-1/2)*(log(2*pi*sigma)+(((x-mu)**2)/(sigma)))
+
+def densmvn(x,y,rho,mux=0,muy=0,sigma1=1,sigma2=2):
+    f = 1/(pi*sigma1*sigma2*np.sqrt(1-rho**2))*exp(-(((x-mux)/sigma1)**2 + 
+        ((y-muy)/sigma2)**2 - 2*rho*(x-mux)*(y-muy)/(sigma1 * sigma2))
+        /(2*(1-rho**2)))
+    return f
 
 def mhbivar(iterations,mu=[0,0],rho=0.8,starting_point=np.array([0,0],dtype=float)):
     '''Implementation of Metropolis-Hastings algorithm for exercise 1'''
@@ -24,34 +31,23 @@ def mhbivar(iterations,mu=[0,0],rho=0.8,starting_point=np.array([0,0],dtype=floa
         if u > 0.5:
             proposal = normal.rvs(loc=mu[0]+rho*((sigma1/sigma2)*(current_point[1]-mu[1])),
                                    scale=sigma1*(1-rho**2))
-            # V = logdensmvn(current_point,mu,Sigma)
-            # Vp = logdensmvn(np.array([proposal,current_point[1]],dtype=float),mu,Sigma)
-            # Wp = logdensnorm(proposal,mu[0]+rho*(current_point[1]-mu[1]),1-rho**2)
-            # Wppr = logdensnorm(current_point[0],mu[0]+rho*(proposal-mu[1]),1-rho**2)
-            # unif = np.random.random()
-            # if exp(Vp-V+Wppr-Wp) < unif:
-            #    current_point[0] = proposal
             current_point[0] = proposal.copy()
         else:
             proposal = normal.rvs(loc=mu[1]+rho*((sigma2/sigma1)*(current_point[0]-mu[0])),
                                scale=sigma2*(1-rho**2))
-            # V  = logdensmvn(current_point,mu,Sigma)
-            # Vp = logdensmvn(np.array([current_point[0],proposal],dtype=float),mu,Sigma)
-            # Wp = logdensnorm(proposal,mu[1]+rho*(current_point[0]-mu[0]),1-rho**2)
-            # Wppr = logdensnorm(current_point[1],mu[1]+rho*(proposal-mu[0]),1-rho**2)
-            # unif = np.random.random()
-            #if exp(Vp-V+Wppr-Wp) < unif:
-            #    current_point[1] = proposal
             current_point[1] = proposal.copy()
         output.append(current_point.copy())
     output = np.array(output,dtype=float)
     return output
+
+# def mvn_graph(x,mu,Sigma=np.array([[1,0],[0,1]],dtype=float)):
+
         
 ## For exercise 2
 def logdenspost(alpha,lamda,t,n=20,c=1,b=1):
-    return (n*(log(alpha) + log(lamda)) + (alpha-1)*log(sum(t)) - 
-            lamda*sum(t**alpha) +n*(log(c)-alpha*c+(alpha-1)*log(lamda)
-            +alpha*log(b)-b*lamda-log(gamma(alpha))))
+    return (n*(log(alpha) + log(lamda)) + (alpha-1)*sum(log(t)) - 
+            lamda*sum(t**alpha)-alpha*c+(alpha-1)*log(lamda)
+            +alpha*log(b)-b*lamda-log(gamma(alpha)))
 
 def loggammadens(x,alpha,beta):
     return (-log(gamma(alpha)) + alpha*log(beta) + (alpha-1)*log(x) - beta*x)
@@ -61,51 +57,43 @@ def MHposter(data,iterations=1000,starting_point=[1,1],n=20,b=1,c=1,sigma=1):
     output = []
     current_point = np.array(starting_point,dtype=float)
     data = np.array(data,dtype=float)
-    alpha = current_point[0]
-    lamda = current_point[1]
+    output.append(current_point)
     for t in range(iterations):
         alpha = current_point[0]
         lamda = current_point[1]
         u = np.random.random()
         if u < 0.25:
             proposal =  gammavar.rvs(a=alpha+n,scale=1/(b+sum(data**alpha)))
-            V  = logdenspost(alpha,proposal,data,n)
-            Vp = logdenspost(alpha,lamda,data,n)
-            W  = loggammadens(lamda,alpha+n,b+sum(data**alpha))
-            Wp = loggammadens(proposal,alpha+n,b+sum(data**proposal))
-            unif = np.random.random()
-            if exp(V-Vp+Wp-W) > unif:
-                current_point[1] = proposal.copy()
+            current_point[1] = proposal.copy()
         elif u >= 0.25 and u < 0.5:
-            r = np.prod(data)
-            if c -log(b) - log(r) > 0:
-                proposal = gammavar.rvs(n+1,-log(b)-log(r)+c)
-                V  = logdenspost(proposal,lamda,data,n)
-                Vp = logdenspost(alpha,lamda,data,n)
-                W  = loggammadens(proposal,n+1,-log(b)-log(r)+c)
-                Wp = loggammadens(lamda,n+1,-log(b)-log(r)+c)
+            if c -log(b) - sum(log(data)) > 0:
+                proposal = gammavar.rvs(a=n+1,scale=1/(-log(b)-sum(log(data))+c))
+                Vp = logdenspost(proposal,lamda,data,n)
+                V  = logdenspost(alpha,lamda,data,n)
+                Wp = loggammadens(proposal,n+1,-log(b)-sum(log(data))+c)
+                W  = loggammadens(alpha,n+1,-log(b)-sum(log(data))+c)
                 unif = np.random.random()
-                if exp(V-Vp+Wp-W) > unif:
+                if log(unif) < Vp-V+W-Wp:
                     current_point[0] = proposal.copy()
             else:
                 pass
         elif u >= 0.5 and u < 0.75:
-            alpha_p = expon.rvs(c)
-            lamda_p = gammavar.rvs(alpha_p,b)
-            V  = logdenspost(alpha_p,lamda_p,data,n)
-            Vp = logdenspost(alpha,lamda,data,n)
-            W  = loggammadens(lamda_p,alpha_p,b)*(log(c) - c*alpha_p)
-            Wp = loggammadens(lamda,alpha,b)*(log(c) - c*alpha)
+            alpha_p = expon.rvs(0)
+            lamda_p = gammavar.rvs(a=alpha_p,scale=1/b)
+            Vp = logdenspost(alpha_p, lamda_p, data, n)
+            V  = logdenspost(alpha,   lamda,   data, n)
+            Wp = loggammadens(lamda_p, alpha_p, b) + (log(c) - c*alpha_p)
+            W  = loggammadens(lamda,   alpha,   b) + (log(c) - c*alpha)
             unif = np.random.random()
-            if exp(V-Vp+Wp-W) > unif:
-                current_point = np.array([alpha_p,lamda_p],dtype=float) 
+            if log(unif) < Vp-V+W-Wp:
+                current_point = np.array([alpha_p,lamda_p],dtype=float)
         else:
             epsilon = normal.rvs(loc=0,scale=sigma)
             proposal = alpha + epsilon
-            V  = logdenspost(proposal,lamda,data,n)
-            Vp = logdenspost(alpha,lamda,data,n)
+            Vp = logdenspost(proposal,lamda,data,n)
+            V  = logdenspost(alpha,lamda,data,n)
             unif = np.random.random()
-            if V-Vp > log(unif):
+            if log(unif) < Vp-V:
                 current_point[0] = proposal.copy()
         output.append(current_point.copy())
     output = np.array(output,dtype=float)
@@ -147,25 +135,154 @@ if __name__ == "__main__":
     from scipy.special import gamma
     from scipy.stats import gamma as gammavar
     from scipy.stats import expon
+    from scipy.stats import weibull_min 
+    
     import numpy as np
     from numpy.linalg import det
+    from numpy.linalg import inv
     from numpy import sum
     from numpy import log
     from numpy import pi
     from numpy import exp
     from numpy import prod
 
-    np.random.seed(57)
+    np.random.seed(17)
+
+    import matplotlib
+    import matplotlib.pyplot as plt
+    matplotlib.style.use("seaborn-v0_8")
 
     # Exercise 1
 
+    Sigma1 = np.array([[1,0.8],[0.8,1]],dtype=float)
+    Sigma2 = np.array([[1,0.95],[0.95,1]],dtype=float)
+    mvn_sample1 = mhbivar(iterations=10000,mu=[2, 3],rho=0.8)
+    mvn_sample2 = mhbivar(iterations=10000,mu=[3,-2],rho=0.95)
+
+    delta = 0.01
+    mvn1_x = np.arange(-1,5,delta)
+    mvn1_y = np.arange(0,6,delta)
+    X1, Y1 = np.meshgrid(mvn1_x, mvn1_y)
+    Z1 = densmvn(X1,Y1,rho=0.8,mux=2,muy=3,sigma1=1,sigma2=1)
+    plt.contour(X1,Y1,Z1,levels=20,cmap="viridis")
+    plt.plot(mvn_sample1[0,0],mvn_sample1[0,1],'ro',label="Punto inicial")
+    plt.plot(mvn_sample1[:,0],mvn_sample1[:,1],alpha=0.75,marker='.', linewidth=0.5, markersize=0.75)
+    plt.title(r"Densidad normal multivariada con $\rho=0.8$")
+    plt.ylabel(r"$y$")
+    plt.xlabel(r"$x$")
+    plt.legend()
+    plt.savefig("Tarea8/traj11.png")
+    plt.show()
+
+    mvn2_x = np.arange(0,6,delta)
+    mvn2_y = np.arange(-5,1,delta)
+    X2, Y2 = np.meshgrid(mvn2_x, mvn2_y)
+    Z2 = densmvn(X2,Y2,rho=0.95,mux=3,muy=-2,sigma1=1,sigma2=1)
+    plt.contour(X2,Y2,Z2,levels=10,cmap="viridis")
+    plt.plot(mvn_sample2[0,0],mvn_sample2[0,1],'ro',label="Punto inicial")
+    plt.plot(mvn_sample2[:,0],mvn_sample2[:,1],alpha=0.75,marker='.', linewidth=0.5, markersize=0.75)
+    plt.title(r"Densidad normal multivariada con $\rho=0.95$")
+    plt.ylabel(r"$y$")
+    plt.xlabel(r"$x$")
+    plt.legend()
+    plt.savefig("Tarea8/traj12.png")
+    plt.show()
+
+    axis = np.linspace(-5,5,500)
+    label1 = r"Marginal en $x$ para $\rho=0.8$, $\mu=2$"
+    label2 = r"Marginal en $y$ para $\rho=0.8$, $\mu=3$"
+    label3 = r"Marginal en $x$ para $\rho=0.95$, $\mu=3$"
+    label4 = r"Marginal en $y$ para $\rho=0.95$, $\mu=-2$"
+    plt.hist(mvn_sample1[:,0],density=True,bins=50,label=label1,alpha=0.8)
+    yaxis1 = normal.pdf(axis,loc=2,scale=np.sqrt(1-0.8**2))
+    yaxis2 = normal.pdf(axis,loc=3,scale=np.sqrt(1-0.8**2))
+    yaxis3 = normal.pdf(axis,loc=3,scale=np.sqrt(1-0.95**2))
+    yaxis4 = normal.pdf(axis,loc=-2,scale=np.sqrt(1-0.95**2))
+    plt.hist(mvn_sample1[:,1],density=True,bins=50,label=label2,alpha=0.8)
+    plt.hist(mvn_sample2[:,0],density=True,bins=50,label=label3,alpha=0.8)
+    plt.hist(mvn_sample2[:,1],density=True,bins=50,label=label4,alpha=0.8)
+    plt.plot(axis,yaxis1)
+    plt.plot(axis,yaxis2)
+    plt.plot(axis,yaxis3)
+    plt.plot(axis,yaxis4)
+    plt.title(r"Histogramas de las marginales")
+    plt.legend()
+    plt.savefig("Tarea8/hist_dens.png")
+    plt.show()
+
+    # Burn-in
+    logtrajex1 = []
+    sigma1 = np.array([[1,  0.8], [0.8,  1]], dtype=float)
+    for i in range(int(np.shape(mvn_sample1)[0])):
+        x = np.array([mvn_sample1[i,0],mvn_sample1[i,1]],dtype=float)
+        logpoint = logdensmvn(x,mu=[2,3],Sigma=sigma1)
+        logtrajex1.append(logpoint)
+    logtrajex1 = np.array(logtrajex1,dtype=float)
+    plt.plot(logtrajex1)
+    plt.xlabel("Iteraciones")
+    plt.ylabel("logaritmo de la densidad")
+    plt.title(r"Trayectoria para burn-in del ejercicio 1 con $\rho=0.8$")
+    plt.savefig("burnin1.png")
+    plt.show()
+
+    logtrajex2 = []
+    sigma2 = np.array([[1, 0.95], [0.95, 1]], dtype=float)
+    for i in range(int(np.shape(mvn_sample2)[0])):
+        x = np.array([mvn_sample2[i,0],mvn_sample2[i,1]],dtype=float)
+        logpoint = logdensmvn(x,mu=[3,-2],Sigma=sigma2)
+        logtrajex2.append(logpoint)
+    logtrajex2 = np.array(logtrajex2,dtype=float)
+    plt.plot(logtrajex2)
+    plt.xlabel("Iteraciones")
+    plt.ylabel("logaritmo de la densidad")
+    plt.title(r"Trayectoria para burn-in del ejercicio 1 con $\rho=0.95$")
+    plt.savefig("burnin2.png")
+    plt.show()
+
 
     # Exercise 2
-    data = simweibull(1,1,20)
-    fail_sims = MHposter(data,10000)
-    plt.plot(fail_sims[:,0],fail_sims[:,1])
+    data = weibull_min.rvs(c=1,size=20)
+    fail_sims = MHposter(data,50000)
+    print(np.mean(fail_sims[:,0]),np.mean(fail_sims[:,1]))
+    plt.plot(fail_sims[:,0],fail_sims[:,1],alpha=0.75,marker='.',
+             linewidth=0.5, markersize=0.75)
+    plt.plot(fail_sims[0,0],fail_sims[0,1],'ro',label="Punto inicial")
+    plt.legend()
+    plt.title("Simulación de la cadena MCMC para los tiempos de falla")
+    plt.savefig("Tarea8/traj2.png")
+    plt.show()
+
+    plt.hist(fail_sims[:,0],density=True,bins=50,alpha=0.75)
+    plt.title(r"Histograma para $\alpha$")
+    plt.savefig("Tarea8/hist21.png")
+    plt.show()
+
+    plt.hist(fail_sims[:,1],density=True,bins=50,alpha=0.75,color="teal")
+    plt.title(r"Histograma para $\lambda$")
+    plt.savefig("Tarea8/hist22.png")
+    plt.show()
+
+    # Burn-in
+    logtrajex3 = []
+    for i in range(int(np.shape(fail_sims)[0])):
+        logpoint = logdenspost(alpha=fail_sims[i,0],lamda=fail_sims[i,1],t=data)
+        logtrajex3.append(logpoint)
+    logtrajex3 = np.array(logtrajex3,dtype=float)
+    plt.plot(logtrajex3)
+    plt.xlabel("Iteraciones")
+    plt.ylabel("logaritmo de la densidad")
+    plt.title("Trayectoria para burn-in del ejercicio 2")
+    plt.savefig("burnin3.png")
+    plt.show()
 
 
     # Exercise 3
-    pump_starting_point = np.array([94.32,15.72,62.88,125.76,5.24,31.44,1.05,1.05,2.1,10.48,4e-2],
-                dtype=float)
+    
+    pump_sims = MHpump(iterations=50000)
+    params = ["\lambda_1","\lambda_2","\lambda_3","\lambda_4","\lambda_5","\lambda_6",
+              "\lambda_7","\lambda_8","\lambda_9","\lambda_{10}",r"\beta"]
+    for i in range(11):
+        plt.hist(pump_sims[:,i],density=True,bins=50,alpha=0.75)
+        plt.title(f"Histograma de ${params[i]}$")
+        plt.savefig(f"Tarea8/hist{i}.png")
+        plt.show()
---
+++ Tarea9.py
@@ -0,0 +1,178 @@
+# Funciones
+
+# Problema 1
+
+def logfac(n):
+    n = int(n)
+    result = 0
+    for i in range(n):
+        result += log(i+1)
+    return result
+
+def ecologpdf(x,N,p):
+    alpha = 1
+    beta  = 20
+    m = np.shape(x)[0]
+    logfx = np.array([logfac(int(i)) for i in x],dtype=float)
+    logfNx = np.array([logfac(int(N-j)) for j in x],dtype=float)
+    return (m*logfac(int(N))-sum(logfx)-sum(logfNx)
+        + (alpha-1+sum(x))*log(p)+(beta-1+m*N-sum(x))*log(1-p))
+
+def MHeco(iterations=100):
+    alpha = 1
+    beta  = 20
+    x = np.array([7,7,8,8,9,4,7,5,5,6,9,8,11,7,5,5,7,3,10,3],dtype=float)
+    p = []
+    N = []
+    m = int(np.shape(x)[0])
+    Nmax = 1000
+    current_p = uniform.rvs()
+    current_N = randint.rvs(low=np.max(x),high=Nmax)
+    p.append(current_p)
+    N.append(current_N)
+    for t in range(iterations):
+        unif = uniform.rvs()
+        if unif < 0.2:
+            if m*current_N-sum(x)+beta > 0:
+                proposal = beta_var.rvs(a=sum(x)+alpha,b=m*current_N-sum(x)+beta)
+                current_p = proposal
+            else:
+                pass
+        elif unif >= 0.2 and unif < 0.4:
+            proposal_p = beta_var.rvs(a=alpha,b=beta)
+            proposal_N = randint.rvs(low=0,high=Nmax)
+            V  = ecologpdf(x,current_N,current_p)
+            Vp = ecologpdf(x,proposal_N,proposal_p)
+            W  = beta_var.logpdf(current_p,a=alpha,b=beta)
+            Wp = beta_var.logpdf(proposal_p,a=alpha,b=beta)
+            unif = np.random.random()
+            if log(unif) < Vp - V - Wp + W:
+                current_p = proposal_p
+                current_N = proposal_N
+        elif unif >= 0.4 and unif < 0.6:
+            M = int(Nmax*current_p)+1
+            proposal_N = hypergeom.rvs(Nmax,500,M)
+            V  = ecologpdf(x,current_N,current_p)
+            Vp = ecologpdf(x,proposal_N,current_p)
+            W  = hypergeom.logpmf(current_N,Nmax,500,int(proposal_N))
+            Wp = hypergeom.logpmf(proposal_N,Nmax,500,int(current_N))
+            unif = np.random.random()
+            if log(unif) < Vp - V + W - Wp:
+                current_N = proposal_N
+        elif unif >= 0.6 and unif < 0.8:
+            rate = int(current_N)
+            proposal_N = np.max(x) + poisson.rvs(rate)
+            rate_p = int(proposal_N)
+            V  = ecologpdf(x,current_N,current_p)
+            Vp = ecologpdf(x,proposal_N,current_p)
+            W  = poisson.logpmf(current_N,mu=rate)
+            Wp = poisson.logpmf(proposal_N,mu=rate_p)
+            unif = np.random.random()
+            if log(unif) < Vp - V + W - Wp:
+                current_N = proposal_N
+        else:
+            rule = np.random.random()
+            epsilon = int(1*(rule < 0.5) - 1*(rule >= 0.5))
+            proposal_N = current_N + epsilon
+            V  = ecologpdf(x,current_N,current_p)
+            Vp = ecologpdf(x,proposal_N,current_p)
+            unif = np.random.random()
+            if log(unif) < Vp - V:
+                current_N = proposal_N
+        p.append(current_p)
+        N.append(current_N)
+    p = np.array(p,dtype=float)
+    N = np.array(N,dtype=float)
+    return p, N
+    
+
+# Problema 2
+
+def logmerc(a,b,c,x,y):
+    quo =  (x-a)**2 / 2*b**2
+    lfy = np.array([logfac(i) for i in y],dtype=float)
+    return (log(c)*sum(y) - sum(y*quo) - sum(lfy) - c*sum(exp(-quo))
+            -(((a-35)**2)/(50)) + loggammadens(c,3,3/950) 
+            + loggammadens(b,2,2/5))
+
+def loggammadens(x,alpha,beta):
+    return (-log(gamma(alpha)) + alpha*log(beta) + (alpha-1)*log(x)
+            -beta*x)
+
+def MHmerc(iterations=100,starting_point=[25,3,5000]):
+    starting_point = np.array(starting_point,dtype=float)
+    output = []
+    current_point = starting_point
+    for t in range(iterations):
+        a = current_point[0]
+        b = current_point[1]
+        c = current_point[2]
+        unif = np.random.random()
+        if unif < 0.333:
+            a_p = gamma_var.rvs(a=54,scale=1/2)
+            V = logmerc(a,b,c,X,Y)
+            Vp = logmerc(a_p,b,c,X,Y)
+            unif = np.random.random()
+            if log(unif) < min(Vp - V,0):
+                current_point[0] = a_p
+            current_point[0] = a_p
+        elif unif >= 0.333 and unif < 0.666:
+            b_p = gamma_var.rvs(a=3,scale=1/2)
+            V   = logmerc(a,b,c,X,Y)
+            Vp  = logmerc(a,b_p,c,X,Y)
+            unif = np.random.random()
+            if log(unif) < min(Vp - V,0):
+                current_point[1] = b_p
+            current_point[1] = b_p
+        elif unif >=0.666 and unif < 0.999:
+            epsilon = normal.rvs(loc=0,scale=200)
+            c_p = c + epsilon
+            V   = logmerc(a,b,c,X,Y)
+            Vp  = logmerc(a,b,c_p,X,Y)
+            unif = np.random.random()
+            if log(unif) < Vp - V:
+                current_point[2] = c_p
+        else:
+            pass
+        output.append(current_point.copy())
+    output = np.array(output,dtype=float)
+    return output 
+
+
+
+
+
+
+
+# Ejercicios
+if __name__ == "__main__":
+    import numpy as np
+    from scipy.stats import uniform
+    from scipy.stats import randint
+    from scipy.stats import norm as normal
+    from scipy.stats import beta as beta_var
+    from scipy.stats import hypergeom
+    from scipy.stats import poisson
+    from scipy.stats import gamma as gamma_var
+    from scipy.special import gamma 
+    from numpy import pi
+    from numpy import log
+    from numpy import exp
+    from scipy.special  import factorial
+
+    # Ejercicio 1
+
+
+
+    # Ejercicio 2
+
+    X = np.array([ 25, 18, 19, 51, 16, 59, 16, 54, 52, 16, 31, 31, 54, 26, 19, 13, 59, 48, 54, 23, 50, 59,
+    55, 37, 61, 53, 56, 31, 34, 15, 41, 14, 13, 13, 32, 46, 17, 52, 54, 25, 61, 15, 53, 39, 33, 52, 65,
+    35, 65, 26, 54, 16, 47, 14, 42, 47, 48, 25, 15, 46, 31, 50, 42, 23, 17, 47, 32, 65, 45, 28, 12, 22,
+    30, 36, 33, 16, 39, 50, 13, 23, 50, 34, 19, 46, 43, 56, 52,42, 48, 55, 37, 21, 45, 64, 53, 16, 62,
+    16, 25, 62])
+
+    Y = np.array([1275, 325, 517, 0, 86, 0, 101, 0, 0, 89, 78, 83, 0, 1074, 508, 5, 0, 0, 0, 1447, 0, 0,
+    0, 0, 0, 0, 0, 87, 7, 37, 0, 15, 5, 6, 35, 0, 158, 0, 0, 1349, 0, 35, 0, 0, 12, 0, 0, 2, 0, 1117, 0,
+    79, 0, 13, 0, 0, 0, 1334, 56, 0, 81, 0, 0, 1480, 177, 0, 29, 0, 0, 551, 0, 1338, 196, 0, 9, 104, 0,
+    0, 3, 1430, 0, 2, 492, 0, 0, 0, 0, 0, 0, 0, 0, 1057, 0, 0, 0, 68, 0, 87, 1362, 0])
